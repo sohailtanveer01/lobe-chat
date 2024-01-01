9ee18985510d971fa764ccbe7eba403c4b932a6b
sohail tanveer <75659484+sohailtanveer01@users.noreply.github.com>
2023-12-31 21:46:14 -0600

    reverting back to original

--- src/app/chat/(desktop)/layout.desktop.tsx
+++ src/app/chat/(desktop)/layout.desktop.tsx
@@ -2,7 +2,7 @@
 
 import { PropsWithChildren, memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
-
+import ResponsiveSessionList from './features/SessionList';
 import AppLayoutDesktop from '@/layout/AppLayout.desktop';
 import { useSwitchSideBarOnInit } from '@/store/global';
 import { SidebarTabKey } from '@/store/global/initialState';
@@ -11,6 +11,7 @@ export default memo(({ children }: PropsWithChildren) => {
   useSwitchSideBarOnInit(SidebarTabKey.Chat);
   return (
     <AppLayoutDesktop>
+      <ResponsiveSessionList /> 
       <Flexbox
         flex={1}
         height={'100%'}
